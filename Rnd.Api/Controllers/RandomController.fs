59903d236c165ac423c24c50383dea863c2e2d04
Eliwuu <marek.wilk@gmail.com>
2022-01-20 16:07:14 +0100

    Add project files.

---
+++ Rnd.Api/Controllers/RandomController.fs
@@ -0,0 +1,17 @@
+﻿namespace Rnd.Api.Controller
+
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+
+[<ApiController>]
+[<Route("[controller]")>] 
+type RandomController (logger : ILogger<RandomController>) =
+    inherit ControllerBase()
+
+    [<HttpGet("one")>]
+    member _.GetOne() = 
+        RandomApi.stream(1)
+
+    [<HttpGet("stream")>]
+    member _.GetStream() =
+        RandomApi.stream(10)
---
+++ Rnd.Api/RandModule.fs
@@ -0,0 +1,16 @@
+﻿module RandomApi
+open System.Security.Cryptography
+open System
+
+let getrand (rng: RandomNumberGenerator) =        
+    let data = Array.zeroCreate(8)
+    rng.GetBytes(data)
+    
+    BitConverter.ToInt32(data)
+        
+let stream x = 
+    let rng = RandomNumberGenerator.Create()
+    seq {
+    for _ in 1..x do  
+    yield getrand rng
+    }
